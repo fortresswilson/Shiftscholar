10b2def9159b41109f2faab5db6b9312319141c1
fortresswilson <108816788+fortresswilson@users.noreply.github.com>
2026-09-22 13:18:12 -0400

    Add initial ShiftScholar homepage

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>ShiftScholar</h1>
+
+      <p>
+        Helping working students balance school, work, and life.
+      </p>
+    </main>
+  );
+}
